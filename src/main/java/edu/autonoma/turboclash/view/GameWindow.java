372321cbd58d34e679314d65da3c38da019ad33f
valerieMCas <valeriemorenocastaneda@gmail.com>
2026-04-03 12:32:51 -0500

    feat(view) se integro la interfaz de gameWindow

--- src/main/java/edu/autonoma/turboclash/view/GameWindow.java
+++ src/main/java/edu/autonoma/turboclash/view/GameWindow.java
@@ -1,11 +1,88 @@
 package edu.autonoma.turboclash.view;
 
 import javax.swing.*;
+import java.util.Random;
 
 public class GameWindow {
     public JPanel panel1;
+    private JLabel Vidas;
+    private JLabel obstaculo;
+    private JLabel monedas;
+    private JLabel Puntaje;
+    private JLabel carro1, carro2, carro3, carro4;
+
+    private Random random = new Random();
+
+    private String[] imagenesObstaculos = {
+            "/image/Barrier.png",
+            "/image/Cone.png",
+            "/image/Oil_Spill.png"
+    };
 
     public GameWindow() {
-        // aquí irá el juego
+
+        panel1.setLayout(null);
+
+        colocarCarros();
+        generarObstaculos(6);
+        generarMonedas(4);
     }
+
+    private void colocarCarros() {
+        carro1 = new JLabel(new ImageIcon(getClass().getResource("/image/Car_Blue.png")));
+        carro2 = new JLabel(new ImageIcon(getClass().getResource("/image/Car_Brown.png")));
+        carro3 = new JLabel(new ImageIcon(getClass().getResource("/image/Car_Red.png")));
+        carro4 = new JLabel(new ImageIcon(getClass().getResource("/image/Car_Yellow.png")));
+
+        carro1.setBounds(50, 100, 100, 50);
+        carro2.setBounds(50, 180, 100, 50);
+        carro3.setBounds(50, 260, 100, 50);
+        carro4.setBounds(50, 340, 100, 50);
+
+        panel1.add(carro1);
+        panel1.add(carro2);
+        panel1.add(carro3);
+        panel1.add(carro4);
+    }
+
+    private void generarObstaculos(int cantidad) {
+
+        int[] carrilesY = {100, 180, 260, 340};
+
+        for (int i = 0; i < cantidad; i++) {
+
+            String ruta = imagenesObstaculos[random.nextInt(imagenesObstaculos.length)];
+
+            JLabel obstaculo = new JLabel(new ImageIcon(getClass().getResource(ruta)));
+            int y = carrilesY[random.nextInt(carrilesY.length)];
+            int x = 400 + (i * 120);
+            obstaculo.setBounds(x, y, 60, 60);
+            panel1.add(obstaculo);
+        }
+
+        panel1.repaint();
+    }
+
+    private void generarMonedas(int cantidad) {
+
+        int[] carrilesY = {100, 180, 260, 340};
+
+        for (int i = 0; i < cantidad; i++) {
+
+            JLabel moneda = new JLabel(
+                    new ImageIcon(getClass().getResource("/image/Coin.png"))
+            );
+
+            int y = carrilesY[random.nextInt(carrilesY.length)];
+            int x = 450 + (i * 150);
+
+            moneda.setBounds(x, y, 40, 40);
+
+            panel1.add(moneda);
+        }
+
+        panel1.repaint();
+    }
+
+
 }
