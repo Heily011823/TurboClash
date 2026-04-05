dfaf39a2b061e8599b624f420872bdeb592e1b94
Maria Paz Puerta Acevedo <mariap.puertaa@autonoma.edu.co>
2026-04-05 01:34:10 -0500

    feat: agregar sonido a las ventanas StarWindow.java e IntroductionWindow.java

--- src/main/java/edu/autonoma/turboclash/validation/MovementValidator.java
+++ src/main/java/edu/autonoma/turboclash/validation/MovementValidator.java
@@ -14,16 +14,19 @@ import edu.autonoma.turboclash.exception.InvalidMovementException;
 public class MovementValidator {
 
     /**
-     * Valida las coordenadas del movimiento.
+     * Valida las coordenadas del movimiento dentro de los límites del mapa.
      *
-     * @param x Es la coordenada en el eje X.
-     * @param y Es la coordenada en el eje Y.
-     * @throws InvalidMovementException Se lanza la excepción si el movimiento está fuera de los límites permitidos.
+     * @param x Coordenada X
+     * @param y Coordenada Y
+     * @param maxX Límite máximo en X
+     * @param maxY Límite máximo en Y
+     * @throws InvalidMovementException Se lanza la excepción si el movimiento está fuera del mapa
      */
-    public static void validate(double x, double y) throws InvalidMovementException {
+    public static void validate(double x, double y, double maxX, double maxY)
+            throws InvalidMovementException {
 
-        if (x < 0 || y < 0) {
-            throw new InvalidMovementException("El movimiento esta fuera del mapa");
+        if (x < 0 || y < 0 || x > maxX || y > maxY) {
+            throw new InvalidMovementException("El movimiento está fuera del mapa");
         }
     }
 }
--- src/main/java/edu/autonoma/turboclash/view/StartWindow.java
+++ src/main/java/edu/autonoma/turboclash/view/StartWindow.java
@@ -1,12 +1,16 @@
 package edu.autonoma.turboclash.view;
 
 import javax.swing.*;
+import edu.autonoma.turboclash.sound.SoundManager;
 
 public class StartWindow {
     public JPanel panel1;
     private JButton btnJugar;
+    private SoundManager soundManager = SoundManager.getInstance();
 
     public StartWindow() {
+
+        SoundManager.getInstance().playBackground(SoundManager.Sound.MENU);
 
         btnJugar.addActionListener(e -> {
 
