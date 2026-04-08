cd4e69aa927668c3325e35f64287cb8d2e51026f
Maria Paz Puerta Acevedo <mariap.puertaa@autonoma.edu.co>
2026-04-07 23:08:48 -0500

    fix: corregir error de los sonidos entre los obtáculos y las monedas

--- src/main/java/edu/autonoma/turboclash/infrastructure/sound/SoundCollisionListener.java
+++ src/main/java/edu/autonoma/turboclash/infrastructure/sound/SoundCollisionListener.java
@@ -2,12 +2,17 @@ package edu.autonoma.turboclash.infrastructure.sound;
 
 import edu.autonoma.turboclash.domain.events.CollisionListener;
 import edu.autonoma.turboclash.domain.model.GameObject;
+import edu.autonoma.turboclash.domain.model.Item;
+import edu.autonoma.turboclash.domain.model.Obstacle;
 
 public class SoundCollisionListener implements CollisionListener {
 
     @Override
     public void onCollision(GameObject object) {
-        SoundManager.getInstance()
-                .playEffect(SoundManager.Sound.BRAKE);
+        if (object instanceof Item) {
+            SoundManager.getInstance().playEffect(SoundManager.Sound.POINT);
+        } else if (object instanceof Obstacle) {
+            SoundManager.getInstance().playEffect(SoundManager.Sound.BRAKE);
+        }
     }
 }
